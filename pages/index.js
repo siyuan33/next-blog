7f0a38475ab818704048b6ae3b6ae2b095a37b0e
DESKTOP-VLDF3F3\Administrator <370455671@qq.com>
2021-02-17 14:55:01 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from "next/head"
-import styles from "../styles/Home.module.css"
+import styles from "styles/Home.module.scss"
 import Link from "next/link"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           <Link href="/post/artical">
